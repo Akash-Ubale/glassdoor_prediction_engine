e322066753360631463d505c9555236c27f5a573
Akash  Ubale <akash.ubale8797@gmail.com>
2021-03-14 14:24:54 +0530

    Started working on glassdoor Scraper

---
+++ glassdoor_scraper.py
@@ -0,0 +1,6 @@
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium import webdriver
+import time
+import pandas as pd
+
+driver = webdriver.Chrome("")
